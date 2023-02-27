0fb784c9747e24c810dcf44fdca0475edfc8322f
EC2 Default User <ec2-user@ip-172-31-37-62.us-east-2.compute.internal>
2023-02-27 20:49:17 +0000

    fix: app port

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,5 +1,5 @@
 export const environment = () => ({
-  port: 3000,
+  port: process.env.APP_PORT || 3001,
   farofaApiUrl: process.env.FAROFA_API_URL || 'http://localhost:4000',
   jwtSecretKey: 'default-secret-env',
   viaCepBaseUrl: process.env.VIA_CEP_BASE_URL || 'https://viacep.com.br/ws',
